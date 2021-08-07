37836fad8c3687b16c1ca5f66513175a4d743676
Kirti Sharma <kirti2001sharma@gmail.com>
2021-08-07 17:36:35 +0530

    fatorial of a number

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factorial{
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int factNum = input.nextInt();
+    int factorial = 1;
+
+    //logic for finding factorial
+
+    for(int i = 1; i <= factNum; i++){
+        factorial *= i; 
+    }
+
+    System.out.println("Factorial of "+factNum + " is " + factorial);
+}
+}
+
